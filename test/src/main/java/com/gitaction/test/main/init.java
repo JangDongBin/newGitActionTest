df32361fe027d7a491789005c8793a479d730540
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-06-06 22:47:00 +0900

    테스트

--- test/src/main/java/com/gitaction/test/main/init.java
+++ test/src/main/java/com/gitaction/test/main/init.java
@@ -1,4 +1,5 @@
-package com.gitaction.test.test;
+package com.gitaction.test.main;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class init {
-    @GetMapping("/")
-    public String init_test() {
+    @GetMapping("/main")
+    public String index() {        
         return "index";
     }
 }
